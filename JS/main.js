b7aaa613304ac7930ede43f8a1309525577c0729
Nuceleus <aaortizreng@gmail.com>
2022-11-08 22:15:16 -0500

    firstExerciseOscript

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+alert("Hello world, i'm learning Otterscript 1")
+alert("Hello world, i'm learning Otterscript 2")
+alert("Hello world, i'm learning Otterscript 3")
+
+//this is a line coment
+
+// Alt + Shift + A  // Método abreviado comentario de bloque
+// Ctrl + } // Método abreviado comentario de línea
+console.log("Hi, this is a console message")
+// data types
+console.log(123456)
+console.log("Augusto")
+console.log(true)
+console.log(false)
+document.write("Hello world this is otterscript")
